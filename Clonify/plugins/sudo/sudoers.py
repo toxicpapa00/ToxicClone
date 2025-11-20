15859841f618b174345bfd66b32e25229a1651ff
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-20 20:56:34 +0530

    Update sudoers.py

--- Clonify/plugins/sudo/sudoers.py
+++ Clonify/plugins/sudo/sudoers.py
@@ -60,7 +60,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("❍ Uff.. 🤪 ●", show_alert=True)
+        return await callback_query.answer("❍ Tᴏxɪᴄ Pᴀᴘᴀ Sᴇ Sᴜᴅᴏ Lᴇ Pᴀʜʟᴇ.. 🤪 ●", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
