958f75cebc4bd1f586dfe42382e9abd9dc46d82c
Gyanesh Patel <gyaneshpatel605@gmail.com>
2025-12-23 09:29:51 +0530

    Update userbot.py

--- Clonify/core/userbot.py
+++ Clonify/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("KITTUU_UPDATES")
-                await self.one.join_chat("KITTUU_UPDATES")
+                await self.one.join_chat("ISHA_BOTS")
+                await self.one.join_chat("ISHA_UPDATES")
             except:
                 pass
             assistants.append(1)
